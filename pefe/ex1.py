de2bf66b9f52fc8e9c34bfe2b1fc0b5c7eb4d591
F <foenyth@gmail.com>
2016-08-13 19:55:30 +0800

    Week 0a - Expressions
    Practice exercises for expressions (optional)

---
+++ pefe/ex1.py
@@ -0,0 +1,15 @@
+# Q: There are 5280 feet in a mile. Write a Python statement that calculates and prints the number of feet in 13 miles.
+
+# Compute the number of feet corresponding to a number of miles.
+
+###################################################
+# Miles to feet conversion formula
+# Student should enter statement on the next line.
+
+print 13 * 5280
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#68640
